d68ab214edcc06c72c0f32fd868627e3c299370b
Ryan Pavlik <rpavlik@iastate.edu>
2011-10-12 17:40:21 -0500

    add heading to test file

--- test/LuaGCBlock.cpp
+++ test/LuaGCBlock.cpp
@@ -1,3 +1,22 @@
+/**	@file
+	@brief	Minimal test of the LuaGCBlock class
+
+	@date	2011
+
+	@author
+	Ryan Pavlik ( <rpavlik@iastate.edu> http://academic.cleardefinition.com/ ),
+	Iowa State University
+	Virtual Reality Applications Center and
+	Human-Computer Interaction Graduate Program
+ */
+
+/*
+ Copyright Iowa State University 2011
+ Distributed under the Boost Software License, Version 1.0.
+ (See accompanying file LICENSE_1_0.txt or copy at
+ http://www.boost.org/LICENSE_1_0.txt)
+ */
+
 
 #include <lua-cpp-utils/LuaGCBlock.h>
 extern "C" {
