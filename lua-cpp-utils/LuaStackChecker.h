fe19b02504aaa903c43fb904f16aa14831c796a0
Ryan Pavlik <rpavlik@iastate.edu>
2011-12-13 10:17:15 -0600

    Move error behavior into a policy class.

--- lua-cpp-utils/LuaStackChecker.h
+++ lua-cpp-utils/LuaStackChecker.h
@@ -34,14 +34,34 @@ extern "C" {
 #endif
 
 // Standard includes
-// - none
+#include <cassert>
 
 
 
 namespace luacpputils {
+	namespace StackCheckerErrorPolicies {
+		/// @brief Error policy for StackChecker that handles errors by raising a Lua error.
+		struct LuaError {
+			static void reportError(lua_State * L, const char * file, int line, int top, int diff, int currentTop) {
+				if (file) {
+					luaL_error(L, "StackChecker error for scope opened at %s:%d: Expected top to be %d + %d, got %d", file, line, top, diff, currentTop);
+				} else {
+					luaL_error(L, "StackChecker error: Expected top to be %d + %d, got %d", top, diff, currentTop);
+				}
+			}
+		};
+
+		/// @brief Error policy for StackChecker that handles errors by using a C assert.
+		struct Assert {
+			static void reportError(lua_State *, const char *, int, int top, int diff, int currentTop) {
+				assert(top + diff == currentTop && "StackChecker error");
+			}
+		};
+	} // end of namespace StackCheckerErrorPolicies
 
 /// Class providing RAII-style verification that you're leaving the Lua stack
 /// the way you want to leave it.
+	template<typename ErrorPolicy = StackCheckerErrorPolicies::LuaError>
 	class StackChecker {
 		public:
 			StackChecker(lua_State * L, int difference = 0)
@@ -59,11 +79,7 @@ namespace luacpputils {
 			~StackChecker() {
 				int currentTop = lua_gettop(_L);
 				if (currentTop != _top + _diff) {
-					if (_file) {
-						luaL_error(_L, "StackChecker error for scope opened at %s:%d: Expected top to be %d + %d, got %d", _file, _line, _top, _diff, currentTop);
-					} else {
-						luaL_error(_L, "StackChecker error: Expected top to be %d + %d, got %d", _top, _diff, currentTop);
-					}
+					ErrorPolicy::reportError(_L, _file, _line, _top, _diff, currentTop);
 				}
 			}
 		private:
